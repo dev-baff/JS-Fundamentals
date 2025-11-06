a8cf5bd62b8a7dd2937e6fc37e56415756894283
baffour amanfo <amanfobaffour16@gmail.com>
2025-11-06 16:21:03 +0000

    Initial commit, factorial created

---
+++ 10-factorial.js
@@ -0,0 +1,15 @@
+const result = parseInt(process.argv[2]);
+
+const factorial = (n) => {
+    if (isNaN(n)) {
+        return 1;
+    }
+
+    if (n <= 1) {
+        return 1;
+    }
+
+    return n * factorial(n - 1);
+};
+
+console.log(factorial(result));
