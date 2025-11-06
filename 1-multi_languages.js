00b98dc78cc5cf6e1ee3e879a0677e6c8681dd6e
baffour amanfo <amanfobaffour16@gmail.com>
2025-11-06 00:30:15 +0000

    Complete task 0, 1-multi_languages.js created

---
+++ 1-multi_languages.js
@@ -0,0 +1,7 @@
+let firstLang = "C is fun";
+let secondLang = "Python is cool";
+let thirdLang = "JavaScript is amazing";
+
+console.log(firstLang);
+console.log(secondLang);
+console.log(thirdLang);
