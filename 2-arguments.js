89fc0a52bdc6793569339fa22e99b31451665a15
baffour amanfo <amanfobaffour16@gmail.com>
2025-11-06 07:40:08 +0000

    Changes made

--- 2-arguments.js
+++ 2-arguments.js
@@ -3,7 +3,7 @@ const ideas = process.argv.length - 2;
 if(ideas === 0) {
     console.log("No argument");
 } else if(ideas === 1) {
-    console.log("Argumment found");
+    console.log("Argument found");
 } else{
     console.log("Arguments found");
 }
